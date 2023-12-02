aacacb726739d04266a5a2c227b8bb72383c965e
sergantik20 <sergejfilatov69724@gmail.com>
2023-12-02 20:24:17 +0300

    creating the foundation of the site

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,505 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="tool">
+    <div class="heading">
+      <header>
+        <h1>
+          <img class="icon" src="@/img.background/pizza.png" />
+          CosmoPizza
+        </h1>
+      </header>
+    </div>
+    <div id="basketDv"><button id="basket">CosmoКорзина | 0р.</button></div>
+    <div id="panelDv">
+      <button id="panel">Пиццы</button>
+      <button id="panel">Роллы</button>
+      <button id="panel">Акции</button>
+      <button id="panel">Напитки</button>
+      <button id="panel">Наша команда</button>
+    </div>
+    <div>
+      <main class="main">
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+      </main>
+
+      <main class="main">
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+        <section class="group_pizza">
+          <article class="card">
+            <div class="blockPizza">
+              <img
+                class="img-pizza"
+                src="@/img.background/pizzacart.png"
+                alt="cheesburger-pizza"
+              />
+              <p>Домашняя</p>
+              <ul class="specifications">
+                <li>
+                  <button class="activeKey">тонкая</button>
+                </li>
+                <li>
+                  <button class="key">традиционная</button>
+                </li>
+              </ul>
+            </div>
+            <ul class="size">
+              <li>
+                <button class="activeKey">26см.</button>
+              </li>
+              <li>
+                <button class="key">30см.</button>
+              </li>
+              <li>
+                <button class="key">40см.</button>
+              </li>
+            </ul>
+          </article>
+          <article class="price-add">
+            <p class="price">
+              от <i><b>359</b></i> &#8381;
+            </p>
+            <button class="add">
+              + Добавить
+              <span class="quantity"> 0 </span>
+            </button>
+          </article>
+        </section>
+      </main>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+}
+.icon {
+  width: 20px;
+  height: 20px;
 }
 
-nav {
-  padding: 30px;
+.tool {
+  background-image: url(img.background/cosmofont.png);
+  background-size: cover;
+  height: 100vh;
+  overflow-x: hidden;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.heading {
+  padding: 30px 40%;
+  text-align: center;
+  height: 35px;
+  background-image: url(img.background/cosmos.jpg);
+  background-size: cover;
+  background-position: 0% 50%;
+  margin: 10px 10px 10px 10px;
+  border: 5px;
+  border-radius: 15px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1 {
+  color: rgb(0, 0, 0);
+  text-align: left;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  display: flex;
+}
+
+#basket {
+  color: white;
+  margin: 10px 10px 10px 10px;
+  height: 30px;
+  width: 30vh;
+  border-radius: 30px;
+  border-color: rgb(40, 0, 65);
+  background-color: rgb(210, 139, 255);
+  font-size: large;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+}
+
+#panel {
+  background-color: aquamarine;
+  margin-left: 20px;
+  padding: 10px 15px;
+  border-color: rgb(0, 191, 0);
+  border-radius: 10px;
+}
+
+#panelDv {
+  width: 200vh;
+  height: 50px;
+  margin-left: 10px;
+  padding-top: 10px;
+  /* background-image: url(panel.png); */
+  border: 3px;
+  border-color: blue;
+  border-radius: 5px;
+}
+.card {
+  width: max-content;
+}
+
+.blockPizza {
+  /* margin-left: -130vh; */
+  margin-top: 50px;
+  display: block;
+  text-align: center;
+  /* font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; */
+  font-size: 20px;
+}
+
+#blockPizza {
+  padding: 5px 18px;
+}
+
+.specifications {
+  margin-top: 10px;
+  padding: 5px;
+  background-color: #bebebe;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  list-style-type: none;
+  border: 3px;
+  border-radius: 10px 10px 0px 0px;
+}
+.size {
+  padding: 5px;
+  background-color: #bebebe;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  list-style-type: none;
+  border: 3px;
+  border-radius: 0px 0px 10px 10px;
+}
+
+.activeKey {
+  width: 100%;
+  height: 25px;
+  align-items: center;
+  padding: 0px 30px;
+  display: flex;
+  background-color: #ffffff;
+  border: 10px;
+  border-radius: 5px;
+}
+.key {
+  align-items: center;
+  padding: 0px 30px;
+  display: flex;
+  border: 3px;
+}
+.price-add {
+  height: 30px;
+  width: 50px;
+  margin-top: 10px;
+  margin-left: 10px;
+  display: flex;
+  white-space: nowrap;
+  font-size: 25px;
+}
+.add {
+  color: #fe5f1e;
+  padding: 0px 15px;
+  margin-left: 15px;
+  border: 3px solid;
+  background-color: #ffffff;
+  border-color: #fe5f1e;
+  border-radius: 10px;
+  font-size: 18px;
+}
+
+.add:hover {
+  background-color: #4e2087;
+  color: white;
+}
+.quantity {
+  padding: 0px 10px;
+  color: white;
+  background-color: #fe5f1e;
+  border-radius: 10px;
+}
+
+.main {
+  justify-content: space-around;
+  display: flex;
 }
 </style>
